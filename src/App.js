50064c646381f0f5444da10293d2de21016b3e96
sungjinahn <fourwings@naver.com>
2020-10-27 13:40:35 +0900

    food사용 기본1 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
+
+const foodLike = [
+  {
+    id : 1,
+    name : '김치',
+    price : 12000,
+    rating : 5
+  },
+  {
+    id :2,
+    name : '감자국',
+    price : 8000,
+    rating : 3
+  },
+  {
+    id : 3,
+    name : '닭볶음',
+    price : 20000,
+    rating : 2
+  }
+];
+
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  price : PropTypes.number.isRequired,
+  rating : PropTypes.number 
+}
+
+
+function Food({name, price, rating}){  
+  return (
+    <>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <p>{rating}/5.0</p>
+      <hr />
+    </>
+  )
+}
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    {
+      foodLike.map((dish,index) => ( 
+        <Food name={dish.name} price={dish.price} rating={dish.rating} key={dish.id} />
+      ))
+    }   
+  </div>
+  )  
 }
 
 export default App;
